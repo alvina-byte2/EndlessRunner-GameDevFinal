938bcb53bb762730036f45340c654e09e5531e92
alvina-byte2 <alvina.1095@bic.edu.pk>
2024-09-01 23:03:20 +0500

    updating player movement
    Finding an issue where when A key is pressed, getting no response. removing the boundary check causes no issue. problem with boundary check.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 2;
     public float horizontalSpeed = 3;
+    public float rightLimit = 5.5f;
+    public float leftLimit = -5.5f;
 
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
-
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed); // This will execute if either the 'A' key or the 'Left Arrow' key is pressed.
+            if (this.gameObject.transform.position.x > leftLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
         }
-
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed); // This will execute if either the 'D' key or the 'Right Arrow' key is pressed.
+            if (this.gameObject.transform.position.x < rightLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            }
         }
-
     }
 }
