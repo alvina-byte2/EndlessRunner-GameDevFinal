70249a0f242cf8e29111ebb79a1ec9ba3992b77a
alvina-byte2 <alvina.1095@bic.edu.pk>
2024-09-09 11:07:42 +0500

    add play button

--- Assets/Scripts/EndRunSequence.cs
+++ Assets/Scripts/EndRunSequence.cs
@@ -15,11 +15,11 @@ public class EndRunSequence : MonoBehaviour
     }
     IEnumerator EndSequence()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         liveCoins.SetActive(false);
         liveDis.SetActive(false);
         endScreen.SetActive(true);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         fadeOut.SetActive(true);
 
     }
---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // This function will be called when the button is clicked
+    public void OnPlayButtonClick()
+    {
+        // Load the next scene in the build index
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
